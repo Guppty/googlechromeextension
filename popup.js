7dd7bd0348fa8c80526f4c706836d25c5a02bf03
Adrian <adricardo679@gmail.com>
2025-02-22 11:46:07 -0800

    fixing bugs

--- popup.js
+++ popup.js
@@ -6,9 +6,9 @@ let counter = 0;
 const interval = setInterval(() => {
     if (counter >= 100) {
         clearInterval(interval); 
-        progressBar.dataset.label = "Success"; // Change text to "Success"
+        progressBar.dataset.label = "Success"; 
     } else {
         counter += 1;
         progressBar.style.setProperty("--width", counter); 
     }
-}, 50); // Adjusted timing for smoother sync
+}, 50); 
